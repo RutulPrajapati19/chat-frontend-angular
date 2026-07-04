5b7ccf5540b647d0f65a1377c36e1ea3a68eb64d
RutulPrajapati19 <rutulprajapati2006@gmail.com>
2026-07-04 17:47:38 +0530

    fix wsUrl missing /ws

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://chat-backend-vdje.onrender.com',
-  wsUrl: 'https://chat-backend-vdje.onrender.com'
+  wsUrl: 'https://chat-backend-vdje.onrender.com/ws'
 };
